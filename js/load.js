188d1bd33b1ddd2795d8886c4bb3465832fa2fc1
Justin Green <JGreen72181@hotmail.com>
2021-04-28 18:31:15 -0700

    un-commmented-out on-load fx()

--- js/load.js
+++ js/load.js
@@ -1,22 +1,22 @@
 let hasClicked = false;
 var mobile = true 
 
-// onload=()=>{
-//     var div = document.createElement("div");
-//         div.className = 'overlay flex center column'
-//         div.innerHTML = "<div class='overlay-bubble text-center'><h1>Cindys <span class='underline'>Checkout</span> </h1><h2>Click to $tart</h2></div>"
-//         document.querySelector(".app").appendChild(div);
-//         setTimeout(()=>{
-//             mobile = confirm("are you on your mobile-device?")
-//         },1000);
+onload=()=>{
+    var div = document.createElement("div");
+        div.className = 'overlay flex center column'
+        div.innerHTML = "<div class='overlay-bubble text-center'><h1>Cindys <span class='underline'>Checkout</span> </h1><h2>Click to $tart</h2></div>"
+        document.querySelector(".app").appendChild(div);
+        setTimeout(()=>{
+            mobile = confirm("are you on your mobile-device?")
+        },1000);
 
-// }
+}
 
 
-// onclick=()=>{
-//     if(hasClicked) return;
-//         document.querySelector(".app").removeChild(document.querySelector(".overlay"))
-//         hasClicked = true;
-// }  
+onclick=()=>{
+    if(hasClicked) return;
+        document.querySelector(".app").removeChild(document.querySelector(".overlay"))
+        hasClicked = true;
+}  
 
 
